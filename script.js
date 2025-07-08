ed01867d6aefb5fecaa202a885240daf2a7306ee
coded <55897154+TeamAbabilCoded@users.noreply.github.com>
2025-07-09 03:06:47 +0700

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const username = tg.initDataUnsafe.user?.username;
 document.getElementById("claimBtn").addEventListener("click", async () => {
   if (!userId) return alert("Gagal ambil ID user.");
 
-  const res = await fetch("https://your-vps-or-domain.com/claim", {
+  const res = await fetch("http://159.89.195.47:8000/add_poin/claim", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify({ user_id: userId, username: username || "" })
